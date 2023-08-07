8b8895985faf2f97042553bcebe9757ffd7a0ac6
Hakan Körhasan <hakankorhasann@gmail.com>
2023-08-07 17:10:12 +0300

    Added comment line for explanations

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -11,8 +11,12 @@ class ViewController: UIViewController {
 
     let mainView = MainView()
     
+    
+    //dataSource adında bir UICollectionViewDiffableDataSource örneği oluşturuluyor. Bu, koleksiyon görünümünün veri kaynağını yönetmek için kullanılacak.
     var dataSource: UICollectionViewDiffableDataSource<Section, User>?
     
+    
+    //Bu fonksiyon, view controller'ın görünümünü mainView ile değiştirir. Bu şekilde, oluşturulan MainView örneği view controller'ın ana görünümü haline gelir.
     override func loadView() {
         view = mainView
     }
@@ -24,8 +28,10 @@ class ViewController: UIViewController {
         getUsers()
     }
     
+    //Bu fonksiyon, koleksiyon görünümünü oluşturur ve yapılandırır. Koleksiyon hücrelerinin görünümü ve içeriği ayarlanır.
     func setupCollectionView() {
         
+        //Bu satırda, koleksiyon hücrelerinin kayıt (registration) işlemi yapılır. Her hücre oluşturulduğunda ne yapılacağı tanımlanır. Hücre içeriğine kullanıcının adı atanır.
         let registration = UICollectionView.CellRegistration<UICollectionViewListCell, User> {
             cell, indexPath, user in
             
@@ -34,6 +40,7 @@ class ViewController: UIViewController {
             cell.contentConfiguration = content
         }
         
+        //Bu satırda, dataSource örneği oluşturulur ve koleksiyon görünümüne bağlanır. Koleksiyon hücrelerinin nasıl dequeued ve yapılandırılacağı tanımlanır.
         dataSource = UICollectionViewDiffableDataSource<Section, User>(collectionView: mainView.collectionView) { (colletionView, indexPath, user) in
             
             colletionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: user)
@@ -52,6 +59,7 @@ class ViewController: UIViewController {
         }
     }
 
+    //Bu fonksiyon, koleksiyon görünümünü günceller. Yeni bir görünüm snapshot'i oluşturur, kullanıcıları ekler ve bu snapshot'i kullanarak dataSource'ı günceller.
     func populate(with users: [User]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, User>()
         snapshot.appendSections([.main])
